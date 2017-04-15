a7ca4a85aa41ab942847455c95ff4b9a64ca9d38
Vikas Rana <vikas@webcontentor.com>
2017-04-15 20:39:31 +0530

    Edit Task date issue fixed

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export function formatDate(params/*, hash*/) {
-  return moment(params[0]).format('MMMM Do YYYY');
+  return moment(params[0]).format('MMMM Do YYYY, hh:mm a');
 }
 
 export default Ember.Helper.helper(formatDate);
